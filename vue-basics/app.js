da4216d43755bd729db146c58edc1f45cda3292b
Tatsuro <contacts@tatsuro.dev>
2019-12-28 18:28:38 +0900

    event objectの参照 10

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -3,10 +3,18 @@ new Vue({
     data: {
         title: 'Becoming a Vue ninja',
         wage: 10,
+        coords: {
+            x: 0,
+            y: 0,
+        },
     },
     methods: {
-        changeWage(amount) {
-            this.wage += amount
+        logEvent(e) {
+            console.log(e)
         },
+        logCoords(e) {
+            this.coords.x = e.offsetX
+            this.coords.y = e.offsetY
+        }
     },
 })

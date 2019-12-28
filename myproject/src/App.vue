fb456fc4a2ec7898f79ef30b60eb8db6cfcff70b
Tatsuro <contacts@tatsuro.dev>
2019-12-29 02:24:57 +0900

    Navbar componentを作成し、root componentで使用 19

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar'
 export default {
   name: 'app',
+  components: {
+    Navbar
+  },
   data() {
     return {
       title: 'My first Vue app, wooo :)'

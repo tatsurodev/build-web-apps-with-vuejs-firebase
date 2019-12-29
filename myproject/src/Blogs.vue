66355e50a4f30ed36c028f2374f22218d081b399
Tatsuro <contacts@tatsuro.dev>
2019-12-30 07:35:48 +0900

    computed property 26

--- myproject/src/Blogs.vue
+++ myproject/src/Blogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="blogs">
     <h2>Blogs</h2>
-    <div v-for="post in posts" :key="post.id">
+    <input type="text" v-model="searchTerm" />
+    <div v-for="post in filteredPosts" :key="post.id">
       <h3>{{ post.title }}</h3>
       <p>{{ post.body | snippet }}</p>
     </div>
@@ -14,13 +15,21 @@ export default {
   name: 'Blogs',
   data() {
     return {
-      posts: []
+      posts: [],
+      searchTerm: ''
     }
   },
   methods: {},
+  computed: {
+    filteredPosts() {
+      return this.posts.filter(post => {
+        return post.title.match(this.searchTerm)
+      })
+    }
+  },
   // dataにaccessできるがdomにはrenderされていないので何も表示はされていない
   created() {
-    alert('created hook')
+    // alert('created hook')
     axios
       .get('https://jsonplaceholder.typicode.com/posts/')
       .then(response => {

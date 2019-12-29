479b4cacf50ce8e59b439224790b2b22d2984540
Tatsuro <contacts@tatsuro.dev>
2019-12-30 08:34:46 +0900

    router link 29

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Navbar />
+    <img src="./assets/logo.png" />
+    <router-view />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
---
+++ routing/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-nav">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'About' }">About</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+</style>

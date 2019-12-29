57e1d05c3aae5f78d2acacbb2ada83a13c6593bb
Tatsuro <contacts@tatsuro.dev>
2019-12-30 05:15:55 +0900

    parent componentからchild componentへpropsでdataを渡す 21

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,19 +2,32 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
+    <AllFriends :friends="friends" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+
 export default {
   name: 'app',
   components: {
-    Navbar
+    Navbar,
+    AllFriends,
+    OnlineFriends
   },
   data() {
     return {
-      title: 'My first Vue app, wooo :)'
+      title: 'My first Vue app, wooo :)',
+      friends: [
+        { name: 'Mario', online: true },
+        { name: 'Luigi', online: false },
+        { name: 'Toad', online: true },
+        { name: 'Bowser', online: false }
+      ]
     }
   }
 }

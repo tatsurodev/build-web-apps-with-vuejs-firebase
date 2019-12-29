2b34e7a3f5361b5ec32de334f72cb68fcba567cb
Tatsuro <contacts@tatsuro.dev>
2019-12-30 06:13:46 +0900

    life cycle hook 23

---
+++ myproject/src/Blogs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="blogs">
+    <h2>{{blogTitle}}</h2>
+    <button @click="changeTitle()">Change Title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blogs',
+  data() {
+    return {
+      blogTitle: 'Blogs'
+    }
+  },
+  methods: {
+    changeTitle() {
+      this.blogTitle = 'Amazing Blog Site'
+    }
+  },
+  // vue instanceが作成された直後、まだdataにはaccessできない
+  beforeCreate() {
+    alert('beforeCreate hook')
+  },
+  // dataにaccessできるがdomにはrenderされていないので何も表示はされていない
+  created() {
+    alert('created hook')
+  },
+  // dataが変更される度hookされる
+  beforeUpdate() {
+    alert('beforeUpdate hook')
+  }
+}
+</script>
+
+<style scoped>
+</style>

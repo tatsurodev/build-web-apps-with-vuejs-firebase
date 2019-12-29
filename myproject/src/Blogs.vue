d2ef6a812e6cbffdf17f58c7802bd28a2012d3ce
Tatsuro <contacts@tatsuro.dev>
2019-12-30 06:33:46 +0900

    created hookとaxiosでapiからdata取得 24

--- myproject/src/Blogs.vue
+++ myproject/src/Blogs.vue
@@ -1,34 +1,35 @@
 <template>
   <div class="blogs">
-    <h2>{{blogTitle}}</h2>
-    <button @click="changeTitle()">Change Title</button>
+    <h2>Blogs</h2>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Blogs',
   data() {
     return {
-      blogTitle: 'Blogs'
+      posts: []
     }
   },
-  methods: {
-    changeTitle() {
-      this.blogTitle = 'Amazing Blog Site'
-    }
-  },
-  // vue instanceが作成された直後、まだdataにはaccessできない
-  beforeCreate() {
-    alert('beforeCreate hook')
-  },
+  methods: {},
   // dataにaccessできるがdomにはrenderされていないので何も表示はされていない
   created() {
     alert('created hook')
-  },
-  // dataが変更される度hookされる
-  beforeUpdate() {
-    alert('beforeUpdate hook')
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts/')
+      .then(response => {
+        console.log(response)
+        this.posts = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>

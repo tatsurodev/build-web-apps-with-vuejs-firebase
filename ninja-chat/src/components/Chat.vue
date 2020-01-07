f2a952a7f06cf45eec184d9d4d2cbc8ebe0a76c9
Tatsuro <contacts@tatsuro.dev>
2020-01-07 09:48:57 +0900

    Chat componentのtemplate作成 64

--- ninja-chat/src/components/Chat.vue
+++ ninja-chat/src/components/Chat.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="chat container">
-    <h2>Chat {{ name }}</h2>
+    <h2 class="center teal-text">Ninja chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">Message</span>
+            <span class="grey-text time">Time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <input type="text" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,4 +29,15 @@ export default {
 </script>
 
 <style>
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span {
+  font-size: 1.4em;
+}
+.chat .time {
+  display: block;
+  font-size: 1.2em;
+}
 </style>

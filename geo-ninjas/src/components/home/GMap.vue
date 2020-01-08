781155b68df254fb23810bb218a1a26960c47367
Tatsuro <contacts@tatsuro.dev>
2020-01-09 00:18:44 +0900

    GMap componentのrenderMap methodにgeolocationのあるuserの緯度経度を表示するmarkerを作成 92

--- geo-ninjas/src/components/home/GMap.vue
+++ geo-ninjas/src/components/home/GMap.vue
@@ -25,6 +25,27 @@ export default {
         minZoom: 3,
         streetViewControl: false
       })
+      db.collection('users')
+        .get()
+        .then(users => {
+          users.docs.forEach(doc => {
+            let data = doc.data()
+            if (data.geolocation) {
+              let marker = new google.maps.Marker({
+                position: {
+                  lat: data.geolocation.lat,
+                  lng: data.geolocation.lng
+                },
+                // map: map
+                map
+              })
+              // markerにclick event追加
+              marker.addListener('click', () => {
+                console.log(doc.id)
+              })
+            }
+          })
+        })
     }
   },
   mounted() {

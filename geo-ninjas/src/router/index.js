86aaaebef6b4080c90b0a7c4a44b66ca46c64bef
Tatsuro <contacts@tatsuro.dev>
2020-01-08 21:51:21 +0900

    各routeに対してroute guardを設定、認証必須で非login userはlogin画面へ 90

--- geo-ninjas/src/router/index.js
+++ geo-ninjas/src/router/index.js
@@ -3,15 +3,19 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes: [
         {
             path: '/',
             name: 'GMap',
-            component: GMap
+            component: GMap,
+            meta: {
+                requiresAuth: true
+            }
         },
         {
             path: '/signup',
@@ -25,3 +29,27 @@ export default new Router({
         }
     ]
 })
+
+// 各routeに対してroute guard
+router.beforeEach((to, from, next) => {
+    // console.log(to.matched)
+    // routeがmetaでauth必須に設定されていたら
+    // $route.matchedで親から子の順番で両方のroute objectを含む配列
+    // some methodで1つでもtrueがあれば全体でtrue
+    if (to.matched.some(rec => rec.meta.requiresAuth)) {
+        // 現user取得
+        let user = firebase.auth().currentUser
+        if (user) {
+            // login済なら
+            next()
+        } else {
+            // login済でないならredirect
+            next({ name: 'Login' })
+        }
+        // auth不要なら
+    } else {
+        next()
+    }
+})
+
+export default router

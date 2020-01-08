b168aad12eafedb2aaf9b0f076507863dd0be194
Tatsuro <contacts@tatsuro.dev>
2020-01-08 13:41:05 +0900

    firebase.authでuser作成、エラーがあればcatch 82

--- geo-ninjas/src/components/auth/Signup.vue
+++ geo-ninjas/src/components/auth/Signup.vue
@@ -25,6 +25,8 @@
 <script>
 import slugify from 'slugify'
 import db from '@/firebase/init'
+// firebase.authの認証用にimport
+import firebase from 'firebase'
 
 export default {
   name: 'Signup',
@@ -40,7 +42,7 @@ export default {
   methods: {
     signup() {
       // aliasが入力されていれば
-      if (this.alias) {
+      if (this.alias && this.email && this.password) {
         this.slug = slugify(this.alias, {
           replacement: '-',
           remove: /[$*_+~.()'"!\-:@]/g,
@@ -55,12 +57,21 @@ export default {
             this.feedback = 'This alias already exists'
             // aliasが存在していない
           } else {
+            // emailとpasswordでuser作成、validation後promiseが返ってくる
+            firebase
+              .auth()
+              .createUserWithEmailAndPassword(this.email, this.password)
+              .catch(err => {
+                console.log(err)
+                // error内容をfeedbackに格納
+                this.feedback = err.message
+              })
             this.feedback = 'This alias is free to use'
           }
         })
         // alias未入力
       } else {
-        this.feedback = 'Please enter an alias'
+        this.feedback = 'Please enter all fields'
       }
     }
   }

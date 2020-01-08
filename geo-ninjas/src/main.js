a77c70d40913ada9f386481e1a1529fa9368fed2
Tatsuro <contacts@tatsuro.dev>
2020-01-08 15:47:47 +0900

    firebase.authの後にvue instanceを作成、firebase.auth().currentUserにaccessできるようにする 84

--- geo-ninjas/src/main.js
+++ geo-ninjas/src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+// vue instanceを格納
+let app = null
+
+// firebase.auth開始後にvue instanceを作成することで、現在のlogin userをfirebase.auth().currentUserで取得できる。firebase.auth開始前にvue instanceを作成すると現在のlogin userを取得できない
+// auth statusが変化する度trigger、vue appが開始されていなければ開始。既に開始されていれば前のinstanceを使用
+firebase.auth().onAuthStateChanged(() => {
+    if (!app) {
+        app = new Vue({
+            el: '#app',
+            router,
+            components: { App },
+            template: '<App/>'
+        })
+
+    }
 })
